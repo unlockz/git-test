37f14261530372d3ecfc34f46acae7b810dc3caa
unlockz <unlockz@126.com>
2022-05-24 20:07:00 +0800

    push commit

--- src/main/java/com/unlockz/HelloGit.java
+++ src/main/java/com/unlockz/HelloGit.java
@@ -12,5 +12,6 @@ public class HelloGit {
         System.out.println("master modify");
         System.out.println("master modify");
         System.out.println("hot-fix modify");
+        System.out.println("push test");
     }
 }
