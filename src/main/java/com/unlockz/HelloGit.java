dce649f087506c3b9904bce7cb9b2ec52a11ff6f
unlockz <unlockz@126.com>
2022-05-24 18:16:51 +0800

    idea second commit

---
+++ src/main/java/com/unlockz/HelloGit.java
@@ -0,0 +1,10 @@
+package com.unlockz;
+
+/**
+ * Created by unlockz on 2022/5/24.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
